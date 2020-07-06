f80c1e4936a2aee5a3f4c188bb1cc751e2220116
PigerGP <PigerGP@users.noreply.github.com>
2020-07-06 19:45:00 +0200

    initialization of class objects
    First version of inits

--- atom.py
+++ atom.py
@@ -1,9 +1,16 @@
 import random
-class AtomBlue():
-        atom_radius = 1
-        def __init__(self):
-            self.m = 1
-            self.v = {'v_x': random.randint(0,10), 'v_y': random.randint(0,10)}
-            
 
-class AtomRed():
+
+class AtomBlue():
+    max_atoms_number = 50
+    radius = 1
+    max_v = 10
+    mass = 1
+
+    def __init__(self):
+        self.v_x, self.v_y = random.randint(-self.max_v, self.max_v), random.randint(-self.max_v, self.max_v)
+
+        # self.x =
+        # self.y =
+
+# class AtomRed():
