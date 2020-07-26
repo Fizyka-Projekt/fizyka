535baaffc031d00c91187e611218eb784e540e62
Jakub Zmuda <65454612+KubaZm@users.noreply.github.com>
2020-07-26 20:25:01 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,3 +6,7 @@ n_L = 20
 height = n_H * radius
 width = n_L * radius
 max_atoms_number = int(0.25 * n_H * n_L)
+k = 10
+dt = 1/(k * max_v) #krok czasu
+M = 50 #ilość odświeżeń
+Dt = M * dt #czas eksperymentu
