207f52f6f24c6bda6141b61e6a4715e6b8376250
Jakub Zmuda <65454612+KubaZm@users.noreply.github.com>
2020-07-26 20:26:02 +0200

    Update animation.py

--- animation.py
+++ animation.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 from atoms import AtomBlue as Atom
 import settings
 
+def draw():
+    plt.scatter([c.x for c in Atom.atoms_list], [c.y for c in Atom.atoms_list], s=100, c='b', marker='o')
+    plt.axis([0, settings.width, 0, settings.height])
+    plt.draw()
+    plt.pause(0.01)
+    plt.clf()
 
-def vis():
-    plt.plot([c.x for c in Atom.atoms_list], [c.y for c in Atom.atoms_list], 'ro')
-    plt.axis('off')
-    plt.show()
