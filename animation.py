37d220309e9c03a1c65c8d37c3b79c3876ab153c
Jakub Zmuda <65454612+KubaZm@users.noreply.github.com>
2020-07-27 14:48:20 +0200

    Update animation.py

--- animation.py
+++ animation.py
@@ -3,8 +3,10 @@ from atoms import AtomBlue as Atom
 import settings
 
 def draw():
+    plt.axes(xlim=(0, settings.width), ylim=(0, settings.height))
     plt.scatter([c.x for c in Atom.atoms_list], [c.y for c in Atom.atoms_list], s=100, c='b', marker='o')
-    plt.axis([0, settings.width, 0, settings.height])
+    plt.axis('off')
+    plt.plot([0,0,settings.width,settings.width,0],[0,settings.height,settings.height,0,0],'-k')
     plt.draw()
     plt.pause(0.01)
     plt.clf()
