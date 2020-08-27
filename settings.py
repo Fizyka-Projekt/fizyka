09688aeefb6a2360ba1cdfc479ac2f9df7d526c8
Miłosz Grocholewski <65454590+Miligro@users.noreply.github.com>
2020-08-27 19:40:36 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,19 @@
 radius = 1
 max_v = 10
 mass = 1
-max_atoms_number = 50  # int(0.25 * min([n_H, n_L]))
 
-n_H = 50
+
+n_H = 20
 n_L = n_H
 height = n_H * radius
 width = n_L * radius
 
+max_atoms_number = 50 #int(0.25 * min([n_H, n_L]))
 atoms_number = 12
 
-k = 4  # min([n_H, n_L])
+k = 20  # min([n_H, n_L])
 dt = 1 / (k * max_v)  # time step
-frames = 30
+frames = 250
 Dt = frames * dt  # total time
 d = radius / 10  # tolerance
 radius_plus = radius + d  # radius with tolerance
